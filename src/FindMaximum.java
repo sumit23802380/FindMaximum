94b3db1fbc7e3ee91a1ba90931005616f3da1430
Sumit Jangid <sumitjangid1692153@gmail.com>
2023-11-26 23:44:49 +0530

    Added : Completed UC 3 Given 3 string find the maximum

--- src/FindMaximum.java
+++ src/FindMaximum.java
@@ -36,6 +36,22 @@ public class FindMaximum {
         }
         return maxElement;
     }
+
+    /*
+     * @desc :Finds the maximum Float number among three elements
+     * @params : float numbers num1 , num2 and num3
+     * @return : maximum float number among three
+     */
+    private static String findMaximum(String num1 , String num2 , String num3){
+        String maxElement = num1;
+        if(maxElement.compareTo(num2)<0){
+            maxElement = num2;
+        }
+        if(maxElement.compareTo(num3)<0){
+            maxElement = num3;
+        }
+        return maxElement;
+    }
     public static void main(String[] args) {
         int testCases=3;
         Scanner sc = new Scanner(System.in);
@@ -53,10 +69,23 @@ public class FindMaximum {
 //            System.out.println("Max Element : " + findMaximum(num1 , num2 , num3));
 //            System.out.println("-------------------------------------------");
 
-            // for use case 1
-            Float num1 = sc.nextFloat();
-            Float num2 = sc.nextFloat();
-            Float num3 = sc.nextFloat();
+//            // for use case 2
+//            Float num1 = sc.nextFloat();
+//            Float num2 = sc.nextFloat();
+//            Float num3 = sc.nextFloat();
+//            sc.nextLine();
+//            System.out.println(" ");
+//            System.out.println("TestCase "+ i +":");
+//            System.out.println("num1 : " + num1);
+//            System.out.println("num2 : " + num2);
+//            System.out.println("num3 : " + num3);
+//            System.out.println("Max Element : " + findMaximum(num1 , num2 , num3));
+//            System.out.println("-------------------------------------------");
+
+//            // for use case 3
+            String num1 = sc.next();
+            String num2 = sc.next();
+            String num3 = sc.next();
             sc.nextLine();
             System.out.println(" ");
             System.out.println("TestCase "+ i +":");
@@ -91,5 +120,15 @@ TestCases added for Use Case 2
 12.95 11.3 5.6
 15.1 22.7 21.0
 17.9 15.3 32.8
+
+TestCases added for Use Case 3
+* TestCase 1 : num1 = abc , num2 = aab , num3 = aaa
+* TestCase 2 : num1 = my , num2 = name , num3 = is
+* TestCase 3 : num1 = hello, num2 = world , num3 = zebra
+
+abc aab aaa
+my name is
+hello world zebra
+
 
  */
