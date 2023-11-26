9ca9c69103e25148f699da472aa285add14b5f4f
Sumit Jangid <sumitjangid1692153@gmail.com>
2023-11-27 00:40:09 +0530

    Updated : Refactor 2 Created one generic class

--- src/FindMaximum.java
+++ src/FindMaximum.java
@@ -5,36 +5,22 @@ import java.util.Scanner;
  * @desc : Finds the maximum
  */
 public class FindMaximum {
-    /*
-     * @desc :Finds the maximum Element among three Generic elements extending Comparable<T>
-     * @params : num1 , num2 and num3
-     * @return : maximum Element among three
-     */
-    private static <T extends Comparable<T>>  T findMaximum(T num1 , T num2 , T num3){
-        T maxElement = num1;
-        if(maxElement.compareTo(num2)<0){
-            maxElement = num2;
-        }
-        if(maxElement.compareTo(num3)<0){
-            maxElement = num3;
-        }
-        return maxElement;
-    }
     public static void main(String[] args) {
         int testCases=3;
         Scanner sc = new Scanner(System.in);
         for(int i=1;i<=testCases;i++){
             // change the name of variable accordingly to get the input
-            String num1 = sc.next();
-            String num2 = sc.next();
-            String num3 = sc.next();
+            Integer num1 = sc.nextInt();
+            Integer num2 = sc.nextInt();
+            Integer num3 = sc.nextInt();
             sc.nextLine();
+            FindMaximumGeneric<Integer> intFindMax = new FindMaximumGeneric<>(num1 , num2 , num3);
             System.out.println(" ");
             System.out.println("TestCase "+ i +":");
             System.out.println("num1 : " + num1);
             System.out.println("num2 : " + num2);
             System.out.println("num3 : " + num3);
-            System.out.println("Max Element : " + findMaximum(num1 , num2 , num3));
+            System.out.println("Max Element : " + intFindMax.testMaximum());
             System.out.println("-------------------------------------------");
         }
         sc.close();
